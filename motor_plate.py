73ac3cdd9b3740587336ac1e8654df435ae321af
swkang99 <kseongwoo99@gmail.com>
2023-11-21 05:54:30 +0900

    [fix] rotate motor

--- motor_plate.py
+++ motor_plate.py
@@ -6,24 +6,37 @@ servo_pin = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin, GPIO.OUT)
 
-pwm = GPIO.PWM(servo_pin, 50)
-pwm.start(2.5)  # 0.6ms
+pwm = GPIO.PWM(servo_pin, 50)  # frequency: 50
+pwm.start(0)
 
 
-def rotate_plate():
-    pwm.ChangeDutyCycle(7.5)  # 7.5 is the duty cycle for 45°
+def set_angle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(servo_pin, True)
+    pwm.ChangeDutyCycle(duty)
     time.sleep(1)
-    pwm.ChangeDutyCycle(2.5)  # 2.5 is the duty cycle for 0°
+    GPIO.output(servo_pin, False)
+    pwm.ChangeDutyCycle(0)
+
+
+def rotate_until(target_angle):
+    rotations = target_angle // 180
+    for _ in range(int(rotations)):
+        # 180도로 회전
+        set_angle(180)
+        time.sleep(1)
+
+    # 남은 각도로 회전
+    remaining_angle = target_angle % 180
+    set_angle(remaining_angle)
 
 
 if __name__ == "__main__":
     try:
         while True:
-            print("Rotate: Plate")
-            rotate_plate()
-            time.sleep(0.5)
+            dest = float(input("Enter angle (greater than 180): "))
+            rotate_until(dest)
 
     except KeyboardInterrupt:
-        print("Measurement stopped by user")
         pwm.stop()
         GPIO.cleanup()
