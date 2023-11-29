29ec9a5ae8758279795408ce2c2768e912fc2282
swkang99 <kseongwoo99@gmail.com>
2023-11-30 08:36:30 +0900

    [feat] 서버에 촬영 이미지 전송

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from ult_wave import get_dist
 from motor import rotate_pillar, rotate_plate
 from time import sleep
-import schedule
-import datetime
+import subprocess
+import requests
+import os
 
-framerate = 60
 cls = -1
 
+server_url = 'http://172.30.1.51:80/detect'
+capture_interval = 1
 
 def measure_capacity():
     """
@@ -14,23 +16,16 @@ def measure_capacity():
     :return:
     """
 
-schedule.every().monday.do(measure_capacity)
 
 if __name__ == "__main__":
-    while cls < 0:
-        print(f'detecting trash . . . ')
-        # take a picture
-        # send picture to server
-        # get predict from DB
-        cls = get_cls()
-
-    while cls >= 0:
-        rotate_pillar(cls)
-        sleep(1)
-        rotate_plate()
-        sleep(1)
-        print(f'detected dist:{get_dist()}')
-
     while True:
-        schedule.run_pending()
-        sleep(1)
+        image_path = 'image.jpg'
+        subprocess.call(['fswebcam', image_path])
+
+        with open(image_path, 'rb') as image_file:
+            response = requests.post(server_url, files={'image': image_file})
+
+        print(response.text)
+        os.remove(image_path)
+
+        time.sleep(capture_interval)
--- motor.py
+++ motor.py
@@ -47,3 +47,4 @@ if __name__ == "__main__":
     sleep(1)
     print("rotate plate2")
     servo_plate.angle = -90
+    sleep(1)
